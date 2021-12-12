7cdaee65930c871cfb67d23e53a619d63e6c76ce
Bruno Vinicius <bruno_v.roger@hotmail.com>
2021-12-12 16:33:27 -0300

    subindo pokedex api

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import pokeapi from '../Img/pokeapi.png'
+
+const Navbar = () =>{
+    return(
+        <nav>
+            <img src={pokeapi} alt="PokeApi" />
+        </nav>
+    )
+        
+}
+
+export default Navbar
